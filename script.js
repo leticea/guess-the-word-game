69c70090448801b3080ffbae8df5e970f82127e9
leticea <nascimento.leticeah@gmail.com>
2023-03-29 11:24:58 -0300

    feat(script.js): js variables

---
+++ script.js
@@ -0,0 +1,8 @@
+const inputs = document.querySelector(".word"),
+  hintTag = document.querySelector(".hint span"),
+  guessLeft = document.querySelector(".guess span"),
+  mistakes = document.querySelector(".wrong span"),
+  resetBtn = document.querySelector(".reset"),
+  hintBtn = document.querySelector(".show-hint"),
+  hintElement = document.querySelector(".hint"),
+  typeInput = document.querySelector(".type-input");
