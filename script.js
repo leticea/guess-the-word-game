ff792ddef53dda905cc7ee26bdf9515553f80628
leticea <nascimento.leticeah@gmail.com>
2023-03-29 11:30:19 -0300

    feat(script.js): startNewGame function

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ const inputs = document.querySelector(".word"),
   hintBtn = document.querySelector(".show-hint"),
   hintElement = document.querySelector(".hint"),
   typeInput = document.querySelector(".type-input");
+
+// Initializing game variables
+let word, incorrectLetters = [], correctLetters = [], maxGuesses;
+
+// Select random word from word list and set up game
+function startNewGame() {
+  alert("New Game Started! Guess New Word :)");
+  // Hide hint element
+  hintElement.style.display = "none";
+  hintElement.style.opacity = "0";
+
+  // Choose random word from db and setup game
+  
+}
